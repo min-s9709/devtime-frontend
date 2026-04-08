44e6a28dfe5bcd0745f37267454248a17164196a
min-s9709 <mim970929@naver.com>
2026-04-08 15:15:46 +0900

    fix: 스크린에 따른 컬럼 분기

--- app/(account)/(registration)/layout.tsx
+++ app/(account)/(registration)/layout.tsx
@@ -7,7 +7,7 @@ export default function RegisterLayout({
 }) {
   return (
     <div className="w-full min-h-screen flex">
-      <section className="w-1/2 bg-primary flex items-center justify-center">
+      <section className="hidden md:flex md:w-1/2 bg-primary items-center justify-center">
         <div className="flex flex-col gap-6 items-center">
           <MainLogo width={264} height={200} className="[&_path]:fill-white" />
           <span className="text-white">
@@ -15,7 +15,7 @@ export default function RegisterLayout({
           </span>
         </div>
       </section>
-      <section className="w-1/2 flex items-center justify-center">
+      <section className="w-full md:w-1/2 flex items-center justify-center">
         {children}
       </section>
     </div>
