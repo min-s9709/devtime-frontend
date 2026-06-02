761e2fe3ed109f67163baafcbf272b2127ded4d6
min-s9709 <mim970929@naver.com>
2026-06-03 01:15:49 +0900

    feat: alert 모달 구현

---
+++ components/common/modal/alert-modal/index.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Button from "@/components/common/button";
+import { useModalStore } from "@/store/use-modal-store";
+
+interface AlertModalProps {
+  title: string;
+  description?: string;
+  onConfirm?: () => void;
+}
+
+export default function AlertModal({ title, description, onConfirm }: AlertModalProps) {
+  const close = useModalStore((state) => state.close);
+
+  const handleConfirm = () => {
+    onConfirm?.();
+    close();
+  };
+
+  return (
+    <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-2">
+        <h2 className="font-semibold text-gray-800 text-title">{title}</h2>
+        {description && (
+          <p className="text-body text-gray-600 whitespace-pre-line">{description}</p>
+        )}
+      </div>
+      <Button variant="Primary" value="확인" onClick={handleConfirm} className="self-end" />
+    </div>
+  );
+}
